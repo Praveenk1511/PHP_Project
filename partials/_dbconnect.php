28b7c09a4e984149e50bc0271bcca512573ae81c
praveen kumar <praveenk15499@gmail.com>
2024-04-17 19:36:04 +0530

    first commit

---
+++ partials/_dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+// Script to connect to the database
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$database = "idiscuss";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+?>
---
+++ partials/_footer.php
@@ -0,0 +1,5 @@
+<div class="container-fluid bg-dark text-light">
+    <p class="text-center">
+        Coprigth iDiscuss Coding Forums 2024 | All rights reserved.
+    </p>
+</div>
